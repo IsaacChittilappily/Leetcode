fb8f8d77f6a255ab7852428f0115cc65d8842154
Isaac Chittilappily <chittilappilyisaac@gmail.com>
2025-03-27 09:29:51 +0000

    Initial solution

---
+++ 258.py
@@ -0,0 +1,9 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+
+        while len(str(num)) != 1:
+            num = sum([int(x) for x in str(num)])
+        
+        return num
+
+print(Solution().addDigits(123))
